1b5171a403019161aa56cbcc38a76583366875a7
prince-tagaev <asaba@internet.ru>
2022-06-04 16:32:18 +0300

    after types change remote autotes

--- src/test/kotlin/WallServiceTest.kt
+++ src/test/kotlin/WallServiceTest.kt
@@ -9,14 +9,14 @@ class WallServiceTest {
         val service = WallService
         // заполняем несколькими постами
         service.add(Post(owner_id = 1, date = 2022, text = "Hi Ko", friends_only = false, post_type = "post",
-            replay_owner_id = 2, signer_id = 2))
+            replay_owner_id = 2, signer_id = 2, views = null, likes = Likes()))
         service.add(Post(owner_id = 1, date = 2022, text = "Hi Kot", friends_only = false, post_type = "post",
-            replay_owner_id = 2, signer_id = 2))
+            replay_owner_id = 2, signer_id = 2, views = null, likes = Likes()))
         service.add(Post(owner_id = 1, date = 2022, text = "Hi Kotl", friends_only = false, post_type = "post",
-            replay_owner_id = 2, signer_id = 2))
+            replay_owner_id = 2, signer_id = 2, views = null, likes = Likes()))
         // создаём информацию об обновлении
         val update = Post(owner_id = 1, date = 2022, text = "Hi Kotli", friends_only = false, post_type = "post",
-            replay_owner_id = 2, signer_id = 2)
+            replay_owner_id = 2, signer_id = 2, views = null, likes = Likes())
 
         // выполняем целевое действие
         val result = service.update(update)
@@ -29,11 +29,11 @@ class WallServiceTest {
     fun add() {
         val service = WallService
         val addPost = service.add(Post(owner_id = 1, date = 2022, text = "Hi Kotl", friends_only = false, post_type = "post",
-            replay_owner_id = 2, signer_id = 2))
+            replay_owner_id = 2, signer_id = 2, views = null, likes = Likes()))
         val addPost2 = service.add(Post(owner_id = 1, date = 2022, text = "Hi Kotl", friends_only = false, post_type = "post",
-            replay_owner_id = 2, signer_id = 2))
+            replay_owner_id = 2, signer_id = 2, views = null, likes = Likes()))
         val addPost3 = service.add(Post(owner_id = 1, date = 2022, text = "Hi Kotl", friends_only = false, post_type = "post",
-            replay_owner_id = 2, signer_id = 2))
+            replay_owner_id = 2, signer_id = 2, views = null, likes = Likes()))
         val result = addPost3.id != 0
         assertTrue(result)
     }
